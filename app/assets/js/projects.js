348bdd5b8b7eb289775f611e86057c1a1907dc56
phothinmg <phothinmg@disroot.org>
2026-03-06 10:14:26 +0630

    modified:   _config.yml

---
+++ app/assets/js/projects.js
@@ -0,0 +1,24 @@
+(function ($) {
+    const projects = [
+        {
+            title: "Susee",
+            summary:
+                "A tiny TypeScript bundler that emits compiled artifacts in multiple module formats.",
+            github: "https://github.com/phothinmg/susee",
+            npm: "https://www.npmjs.com/package/susee",
+            image:
+                " https://pub-c9ba018358dd48a99b70013b65a25e5f.r2.dev/logo/susee.webp",
+        },
+        {
+            title: "Thetkarit",
+            summary:
+                "A library for Burmese calendar calculations and astronomical studies.",
+            github: "https://github.com/phothinmg/thetkarits",
+            image:
+                "https://pub-c9ba018358dd48a99b70013b65a25e5f.r2.dev/logo/thetkarit.webp",
+        },
+    ];
+
+    const pel = $.document.getElementById("pel");
+
+})(window);
